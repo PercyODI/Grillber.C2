41e154044f580bab5d9d4740cc62f83ab96c96f4
PercyODI <pearse.2008@gmail.com>
2019-11-20 09:16:04 -0600

    Adding more endpoints for user management.

--- Grillber.C2/Controllers/UsersController.cs
+++ Grillber.C2/Controllers/UsersController.cs
@@ -52,7 +52,69 @@ namespace Grillber.C2.Controllers
                 return NotFound();
         }
 
+        [HttpPost]
+        public IHttpActionResult Post([FromBody] UserNew newUser)
+        {
+            if (!string.IsNullOrWhiteSpace(newUser.Username))
+            {
+                return BadRequest("Username must be provided.");
+            }
+
+            if (StaticUsers.Any(su => su.Username == newUser.Username))
+            {
+                return BadRequest("Username is already taken.");
+            }
+
+            var createdUser = new UserOut()
+            {
+                Id = Guid.NewGuid(),
+                FirstName = newUser.FirstName,
+                LastName = newUser.LastName,
+                Username = newUser.LastName
+            };
+            StaticUsers.Add(createdUser);
+
+            return Ok(createdUser);
+        }
+
+        [HttpPut]
+        [Route("{userId:Guid}")]
+        public IHttpActionResult Put(Guid userId, [FromBody] UserUpdate updatedUser)
+        {
+            var foundUser = StaticUsers.FirstOrDefault(su => su.Id == userId);
+            if (foundUser == null)
+            {
+                return NotFound();
+            }
+
+            if (!string.IsNullOrWhiteSpace(updatedUser.FirstName))
+            {
+                foundUser.FirstName = updatedUser.FirstName;
+            }
+
+            if (!string.IsNullOrWhiteSpace(updatedUser.LastName))
+            {
+                foundUser.LastName = updatedUser.LastName;
+            }
+
+            return Ok(foundUser);
+        } 
+
     }
+
+    public class UserUpdate
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    public class UserNew
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+    }
+
     public class UserOut
     {
         public Guid Id { get; set; }
